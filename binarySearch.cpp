c3259372d183e8ee32ed807dc090261e0744afbd
sabbirislam111 <sabbirmollah302@gmail.com>
2022-08-26 22:07:49 +0600

    Binary srarch

---
+++ binarySearch.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+    int arr[n];
+
+    for(int i = 0; i < n; i++){
+        cin>>arr[i];
+    }
+
+    int target;
+    cin>>target;
+
+    int lb = 0, ub = n-1;
+    bool found = false;
+    while(lb <= ub){
+        int mid = (lb+ub)/2;
+        if(arr[mid] == target){
+            found = true;
+            cout<<"It is index = "<<mid+1<<endl;
+            break;
+        }
+
+        if(target < arr[mid]){
+            ub = mid-1;
+        }
+        else{
+            lb = mid+1;
+        }
+    }
+    if(found == false){
+        cout<<"Not found"<<endl;
+    }
+
+
+
+
+ return 0;
+}
